696aee6ea6e4c838c23367763ace33c09a19ee18
Beatriz Siqueira <beatrizsiqueiracosta2001@hotmail.com>
2023-03-27 22:53:51 -0300

    feat: :bug: bugs in code

--- 7-http-react/src/App.js
+++ 7-http-react/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [products, setProducts] = useState([]);
+    
+    //1 - resgatando dados
+    useEffect(async () => {
+        const res = await fetch(url);
+        const data = await res.json();
+        setProducts(data);
+    }, []);
+
+    console.log(products);
+
+    return (
+        <div className="App">
+            <h1>Lista de Produtos</h1>
+        </div>
+    );
 }
 
 export default App;
